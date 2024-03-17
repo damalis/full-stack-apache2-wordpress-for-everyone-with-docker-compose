f719ec8b2753d0fa9112455da3dbc372e30750f3
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-17 12:36:40 +0300

    Update entrypoint.sh

--- docker/3.0/entrypoint.sh
+++ docker/3.0/entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Add the FTP_USER, change his password and declare him as the owner of his home folder and all subfolders
-adduser -u 431 -D -G ftp -h /home/vsftpd/$FTP_USER -s /bin/false $FTP_USER
-echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+adduser -u 431 -D -G ftp -h /home/vsftpd/${FTP_USER} -s /bin/false ${FTP_USER}
+echo "${FTP_USER}:${FTP_PASS}" | /usr/sbin/chpasswd
 chown -R ftp:ftp /home/vsftpd/
 
 # Building the configuration file
@@ -14,16 +14,18 @@ VSFTPD_CONF=/etc/vsftpd/vsftpd.conf
 echo "Update the vsftpd.conf according to env variables"
 echo "pasv_enable=$PASV_ENABLE" >> $VSFTPD_CONF
 
-PASV_ADDRESS=$(/sbin/ip route|awk '/src/ { print $7 }')
-echo "pasv_address=$PASV_ADDRESS" >> $VSFTPD_CONF
+#PASV_ADDRESS=$(/sbin/ip route|awk '/src/ { print $7 }')
+#echo "pasv_address=$PASV_ADDRESS" >> $VSFTPD_CONF
 
-echo "pasv_addr_resolve=$PASV_ADDR_RESOLVE" >> $VSFTPD_CONF
-echo "pasv_max_port=$PASV_MAX_PORT" >> $VSFTPD_CONF
-echo "pasv_min_port=$PASV_MIN_PORT" >> $VSFTPD_CONF
-echo "syslog_enable=$SYSLOG_ENABLE" >> $VSFTPD_CONF
-echo "local_root=$LOCAL_ROOT" >> $VSFTPD_CONF
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> $VSFTPD_CONF
+echo "pasv_address=${PASV_ADDRESS}" >> $VSFTPD_CONF
+echo "pasv_min_port=${PASV_MIN_PORT}" >> $VSFTPD_CONF
+echo "pasv_max_port=${PASV_MAX_PORT}" >> $VSFTPD_CONF
+echo "syslog_enable=${SYSLOG_ENABLE}" >> $VSFTPD_CONF
+echo "local_root=${LOCAL_ROOT}" >> $VSFTPD_CONF
 echo "rsa_cert_file=${LETSENCRYPT_CONF_PREFIX}/live/${DOMAIN_NAME}/fullchain.pem" >> $VSFTPD_CONF
 echo "rsa_private_key_file=${LETSENCRYPT_CONF_PREFIX}/live/${DOMAIN_NAME}/privkey.pem" >> $VSFTPD_CONF
+echo "ftpd_banner=Welcome to ${DOMAIN_NAME} FTP service." >> $VSFTPD_CONF
 
 # Run the vsftpd server
 echo "Running vsftpd"
